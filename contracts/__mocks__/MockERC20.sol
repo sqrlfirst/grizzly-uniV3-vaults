59f1dcd24e33b03cf477f36369192f962b73052f
daniecon <daniel.contreras@math.ethz.ch>
2023-02-22 16:37:44 +0100

    fix: updated upgradeable

--- contracts/__mocks__/MockERC20.sol
+++ contracts/__mocks__/MockERC20.sol
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.4;
 
-import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {
+	ERC20Upgradeable
+} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract MockERC20 is ERC20Upgradeable {
-	constructor() {
-		__ERC20_init("", "TOKEN");
+	// constructor() {
+	// 	__ERC20_init("", "TOKEN");
+	// 	_mint(msg.sender, 100000e18);
+	// }
+	function initialize(string memory _tokenName, string memory _tokenSymbol)
+		public
+		initializer
+	{
+		__ERC20_init(_tokenName, _tokenSymbol);
 		_mint(msg.sender, 100000e18);
 	}
 }
+
